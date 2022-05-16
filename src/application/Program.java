e1ab8941a8b398fd9c7f21beb9a3d6ce5e8c291d
Paulo Filho <paulopbalsamo@gmail.com>
2022-05-16 01:24:29 -0300

    SellerDaoJDB DepartamentDaoJDB e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,9 +17,9 @@ public class Program {
 		Seller objSeller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,objDepartment);
 		System.out.println(objSeller);
 		
+		SellerDao sellerDao =  DaoFactory.createSellerDao();
 		
-		
-		
+		System.out.println(objSeller);
 		
 		
 	}
