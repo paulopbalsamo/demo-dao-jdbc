330d464db8a8ef82a4cfb2d0233c9e037d17af29
Paulo Filho <paulopbalsamo@gmail.com>
2022-05-16 01:02:30 -0300

    commit aula Classe Department

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Department objDepartment  = new Department(1,"BOOKS");
+		System.out.println(objDepartment);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
